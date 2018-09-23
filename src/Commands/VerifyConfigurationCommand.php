102dd725b002f822121ecd77a9a792cf743a1157
Leo Sjöberg <leo.sjoberg@gmail.com>
2018-09-23 18:44:58 +0100

    Add initial artisan command

---
+++ src/Commands/VerifyConfigurationCommand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hindsight\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Config\Repository;
+use Psr\Log\LoggerInterface;
+
+class VerifyConfigurationCommand extends Command
+{
+    protected $signature = 'hindsight:verify';
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var Repository
+     */
+    private $config;
+
+    /**
+     * VerifyConfigurationCommand constructor.
+     * @param LoggerInterface $logger
+     * @param Repository      $config
+     */
+    public function __construct(LoggerInterface $logger, Repository $config)
+    {
+        $this->logger = $logger;
+        $this->config = $config;
+    }
+
+    public function handle()
+    {
+        $this->info('Sending log message to Hindsight...');
+        $this->logger->info('Hindsight setup working', [
+            'code' => 'hindsight-verification.success',
+            'configuration' => $this->config->get('hindsight'),
+        ]);
+        $this->info('✔ Log message transmitted to Hindsight');
+    }
+}
